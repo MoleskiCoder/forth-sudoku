a19eb48e7778c80b6de2ab869b9d92d4d0f6b1b0
Adrian Conlon <adrian.conlon@gmail.com>
2015-09-20 19:25:08 +0100

    Add a couple of "box" position translators.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- sudoku.fs
+++ sudoku.fs
@@ -129,16 +129,22 @@ create possible-moves
 : box>box-xy ( box-n -- box-x box-y )
    box-size /mod ;
 
+: box-xy>offset ( box-x box-y -- offset )
+   box-size * + ;
+
 : box>first-move ( box -- n )
    box>box-xy
    board-size box-size * * 
-   swap box-size * + ;
+   swap box-xy>offset ;
 
 : box-offset>move-offset ( n -- offset )
    box>box-xy xy>move ;
 
 : box-offset>move ( box offset -- n )
    box-offset>move-offset swap box>first-move + ;
+
+: box-box-xy>move ( box box-x box-y -- n )
+   box-xy>offset box-offset>move ;
 
 
 ( Possible handlers.  Possible set is an array of bitsets )
