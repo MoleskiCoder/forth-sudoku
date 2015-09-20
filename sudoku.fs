7b7d11f33d0266e6caa32ab75cfe963b258cb8f6
Adrian Conlon <adrian.conlon@gmail.com>
2015-09-20 11:21:22 +0100

    Ad skeletal "candidate lines" eliminator.  Skeletal, but benign.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- sudoku.fs
+++ sudoku.fs
@@ -438,6 +438,54 @@ variable dangling
      i eliminate-all-box-dangling
    loop ;
 
+( "Candidate Lines" )
+
+create eliminator-data 3 cells allot
+
+: erase-eliminator-data
+   eliminator-data 3 cells erase ;
+
+: eliminate-box-candidate-row-value ( box row value -- )
+   erase-eliminator-data
+   drop drop drop ;
+
+: eliminate-box-candidate-column-value ( box column value -- )
+   erase-eliminator-data
+   drop drop drop ;
+
+: eliminate-box-candidate-row ( box row -- )
+   board-size 0 do
+     over over
+     i eliminate-box-candidate-row-value
+   loop 2drop ;
+
+: eliminate-box-candidate-column ( box column -- )
+   board-size 0 do
+     over over
+     i eliminate-box-candidate-column-value
+   loop 2drop ;
+
+: eliminate-box-candidate-rows ( box -- )
+   box-size 0 do
+     dup i eliminate-box-candidate-row
+   loop drop ;
+
+: eliminate-box-candidate-columns ( box -- )
+   box-size 0 do
+     dup i eliminate-box-candidate-column
+   loop drop ;
+
+: eliminate-box-candidate-lines ( box -- )
+   dup
+   eliminate-box-candidate-rows
+   eliminate-box-candidate-columns ;
+
+: eliminate-all-candidate-lines ( -- )
+   board-size 0 do
+     i eliminate-box-candidate-lines
+   loop ;
+
+
 ( -------------- Easy ------------------ )
 
 ( "Single Position" *Done*
@@ -563,7 +611,8 @@ variable dangling
    eliminate-all-row-possibilities
    eliminate-all-column-possibilities
    eliminate-all-box-possibilities 
-   eliminate-all-dangling ;
+   eliminate-all-dangling
+   eliminate-all-candidate-lines ;
 
 : solve ( -- )
    begin
